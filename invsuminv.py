b76e12352a55bbfca7337877d444f1160fb0f295
Joshua Holbrook <josh.holbrook@gmail.com>
2010-01-04 15:56:55 -0900

    fixed isopleth promblem with invsuminv

--- invsuminv.py
+++ invsuminv.py
@@ -1,35 +1,40 @@
-### Type7-Isopleths.py ###
+### invsuminv.py
+### Adopted from http://www.myreckonings.com/pynomo11/Type7-Isopleths.py
 
 from pynomo.nomographer import *
  
 R1_params={
       'u_min':0.0,
-      'u_max':1000.0,
+      'u_max':10.0,
       'function':lambda u:u,
-      'title':r'\Large $R_1$',
-      'text_format':r"$%4.0f$",
+      'title':r'\Large $x_{(1,6,10\ldots)}$',
+      'text_format':r"$%4.2f$",
+      'title_x_shift':-0.9,
       'tick_levels':4,
       'tick_text_levels':2,
              }
  
 R2_params={
-      'u_min':5.0,
-      'u_max':1000.0,
+     'u_min':0.005,
+      'u_max':10.0,
       'function':lambda u:u,
-      'title':r'\Large $R_2$',
-      'text_format':r"$%4.0f$",
-      'title_y_shift':0.5,
+      'title':r'\Large $x_{(2,5,9\ldots)}$',
+      'text_format':r"$%4.2f$",
+      'title_x_shift':-0.5,
+      'title_y_shift':0.4,
       'tick_levels':4,
       'tick_text_levels':2,
              }
  
 R1R2_params_1={
       'tag':'r1r2',
-      'u_min':5.0,
-      'u_max':1000.0,
+      'u_min':0.005,
+      'u_max':10.0,
       'function':lambda u:u,
-      'title':r'\Large $R_1 \parallel R_2$',
-      'text_format':r"$%4.0f$",
+      'title':r'\Large $x_{(3,7,11\ldots)}$',
+      'text_format':r"$%4.2f$",
+      'title_x_shift':1.0,
+      'title_y_shift':0.1,
       'tick_levels':4,
       'tick_text_levels':2,
              }
@@ -44,37 +49,37 @@ block_1_params={
       'f1_params':R1_params,
       'f2_params':R2_params,
       'f3_params':R1R2_params_1,
-      'isopleth_values':[[650,900,'x']],
+      'isopleth_values':[[6.50,9.00,'x']],
              }
 
 R1R2_params_2={
       'tag':'r1r2',
       'u_min':0.0,
-      'u_max':1000.0,
+      'u_max':10.0,
       'function':lambda u:u,
       'title':r'',
-      'text_format':r"$%4.0f$",
+      'text_format':r"$%4.2f$",
       'tick_levels':0,
       'tick_text_levels':0,
              }
  
 R3_params={
       'u_min':0.0,
-      'u_max':1000.0,
+      'u_max':10.0,
       'function':lambda u:u,
-      'title':r'\Large $R_3$',
-      'text_format':r"$%4.0f$",
-      'title_x_shift':-10.0,
+      'title':r'\Large $x_{(4,8,12\ldots)}$',
+      'text_format':r"$%4.2f$",
+      'title_x_shift':-9.1,
       'tick_levels':4,
       'tick_text_levels':2,
              }
  
 R1R2R3_params={
       'u_min':0.0,
-      'u_max':1000.0,
+      'u_max':10.0,
       'function':lambda u:u,
-      'title':r'\Large $R_1 \parallel R_2 \parallel R_3$',
-      'text_format':r"$%4.0f$",
+      'title':r'',
+      'text_format':r"$%4.1f$",
       'title_x_shift':0.5,
       'title_y_shift':-4.0,
       'tick_levels':0,
@@ -91,11 +96,11 @@ block_2_params={
       'f1_params':R1R2_params_2,
       'f2_params':R3_params,
       'f3_params':R1R2R3_params,
-      'isopleth_values':[['x',600,'x']],
+      'isopleth_values':[['x',6.00,'x']],
              }
  
 main_params={
-      'filename':'Type7-Isopleths.pdf',
+      'filename':'invsuminv.eps',
       'paper_height':20.0,
       'paper_width':20.0,
       'block_params':[block_1_params,block_2_params],
@@ -103,22 +108,24 @@ main_params={
       'title_x': 0.0,
       'title_y': -3.0,
       'title_box_width': 12,
-      'title_str':r'\Large Equivalent Resistance of 3\
-                    Resistors in Parallel',
+      'title_str':r'\Large $1/x_{(1,6,10\ldots)} + 1/x_{(2,5,9\ldots)} = \
+      1/x_{(3,7,11\ldots)}$',
       'extra_texts':[
-            {'x':-3.5,
+            {'x':-4.9,
              'y':-3.7,
-             'text':r'\large $1/R = 1/R_1 + 1/R_2 + 1/R_3$',
-             'width':12.0,
+             'text':r'Useful for such calculations as \
+             resistors in parallel, capacitors in series, or thermal \
+             insulators (R-values) in parallel.',
+             'width':10.0,
             }],
-      'isopleth_params':[
-           {'color':'Red',
-            'linewidth':'thin',
-            'linestyle':'dashed',
-            'circle_size':0.0,
-            'transparency':0.0,
-           },
-           ],
+      #'isopleth_params':[
+      #     {'color':'Red',
+      #      'linewidth':'thin',
+      #      'linestyle':'dashed',
+      #      'circle_size':0.0,
+      #      'transparency':0.0,
+      #     },
+      #     ],
               }
 
 Nomographer(main_params)
