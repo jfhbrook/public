83f1cd0ed1fd4e85ca6849223b7373db8981e64a
Joshua Holbrook <josh.holbrook@gmail.com>
2010-02-11 12:13:15 -0700

    I forgot to commit some changes, it seems.

--- libtsl.py
+++ libtsl.py
@@ -1,5 +1,17 @@
 from math import pi
 
+#data LaTeX formatting
+def textable(columns,titles):
+    print r'\begin{tabular}{*{'+str(len(columns))+r'}{l}}'
+    print ' & '.join(titles), r'\\'
+    print r'\hline'
+    for row in zip(*columns):
+        strrow=[]
+        for element in row:
+            strrow.append(str(element))
+        print ' & '.join(strrow), r'\\'
+    print r'\end{tabular}','\n'
+
 #data manipulations
 def subset(set,n):
     sub=[]
@@ -8,6 +20,7 @@ def subset(set,n):
             sub=sub+[set[i]]
     return sub
 
+#sphere geometry
 class sphere:
     def __init__(self,d):
         self.d=d
@@ -27,34 +40,34 @@ class htparams:
         self.Tinf=args.get('Tinf',273.15)
 
         self.kvisc=args.get('kvisc',False)
-        self.rho=args.get('rho',False)
+        self.rho_f=args.get('rho_f',False)
+        self.rho_s=args.get('rho_s',False)
         self.dvisc=args.get('dvisc',False)
-        if(not self.kvisc and self.rho and self.dvisc):
-            self.kvisc=self.dvisc/self.rho
-        if(not self.dvisc and self.rho and self.kvisc):
-            self.dvisc=self.kvisc*self.rho
-        self.k=args.get('k',False)
-        self.cp=args.get('cp',False)
+        if(not self.kvisc and self.rho_f and self.dvisc):
+            self.kvisc=self.dvisc/self.rho_f
+        if(not self.dvisc and self.rho_f and self.kvisc):
+            self.dvisc=self.kvisc*self.rho_f
+        self.k_f=args.get('k_f',False)
+        self.k_s=args.get('k_s',False)
+        self.cp_f=args.get('cp_f',False)
+        self.cp_s=args.get('cp_s',False)
         self.alpha=args.get('alpha',False)
-        if (not self.alpha and self.rho and self.cp and self.k):
-            self.alpha=self.k/(self.rho*self.cp)
+        if (not self.alpha and self.rho_f and self.cp_f and self.k_f):
+            self.alpha=self.k_f/(self.rho_f*self.cp_f)
         self.h=args.get('h',False)
         self.emiss=args.get('emiss',1.0)
-        self.thermexp=args.get('thermexp',1.0)
+        self.thermexp=args.get('thermexp',1.0/self.Tinf)
 
 #    def biot(self)
 #        return h*l/k
 
     def rayleigh(self,Tf):
-        print 'Ra=', self.grashof(Tf)*self.prandtl(Tf)
         return self.grashof(Tf)*self.prandtl(Tf)
 
     def grashof(self,T):
-        print 'Gr=', self.g * self.thermexp * (T-self.Tinf)*(self.l**3.0)/(self.kvisc**2)
-        return (self.g*self.thermexp*(T-self.Tinf)*self.l**3.0)/(self.kvisc**2)
+        return (self.g*self.thermexp*(T-self.Tinf)*((self.l)**3.0))/((self.kvisc)**2.0)
 
     def prandtl(self,T):
-        print 'Pr=', self.kvisc/self.alpha
         return self.kvisc/self.alpha
 
     def reynolds(self):
@@ -66,12 +79,10 @@ class htparams:
     #specific to metal sphere
     def hc(self,Tzero,Tfinal):
         #Incropera and Dewitt's empirical equation for hc
-        #May depend on Englilish units.
         Tf=self.Tfilm(Tzero,Tfinal)
-        return (self.k/self.l)*((0.589*self.rayleigh(Tf)**0.25)/(1+(0.469/self.prandtl(Tf))**(9.0/16.0))**(4.0/9.0)+2)
+        return (2+(0.589*(self.rayleigh(Tf))**0.25)/(1+(0.469/self.prandtl(Tf))**(9.0/16.0))**(4.0/9.0))*(self.k_f/self.l)
 
     def hr(self,Tsurf):
         #Don't forget to use absolute temps!
-        stefboltz=5.6704e-5/1055.0559/1550.0031/1.8 #thx units as well
-        #thx http://en.wikipedia.org/wiki/Stefan%E2%80%93Boltzmann_constant
+        stefboltz=0.1714e-8/3600.0 #courtesy cengel
         return self.emiss*stefboltz*(Tsurf**4.0-self.Tinf**4.0)/(Tsurf-self.Tinf)
