a50dba5b7e75608d7604b1727c9aa5464cce3350
Joshua Holbrook <josh.holbrook@gmail.com>
2010-02-11 23:08:57 -0900

    added a viscometer table, a csv datapoint reader and an excel table outputter

--- libtsl.py
+++ libtsl.py
@@ -1,6 +1,52 @@
 from math import pi
+import xlwt, xlrd
+import csv
 
-#data manipulations
+###data input
+
+#Reads in data points
+def csvdata(filename):
+    datareader=csv.reader(open(filename))
+    titles=datareader.next()
+    datalists=[]
+    for row in datareader:
+        datalists.append(row)
+    print titles
+    #some outrageous shit >_<
+    return dict(zip(titles,
+                    [list(tupl) for tupl in zip(*datalists)]))
+
+###data output
+
+#data LaTeX formatting
+#copy-paste or pipe
+def textable(columns,titles):
+    print r'\begin{tabular}{*{'+str(len(columns))+r'}{l}}'
+    print ' & '.join(titles), r'\\'
+    print r'\hline'
+    for row in zip(*columns):
+        strrow=[]
+        for element in row:
+            strrow.append(str(element))
+        print ' & '.join(strrow), r'\\'
+    print r'\end{tabular}','\n'
+
+#data excel formatting
+#saves to file
+#UNTESTED
+def exceltable(columns,titles,filename):
+    workbook=xlwt.Workbook()
+    sheet=workbook.add_sheet("Some Table or Other")
+    for i, entry in enumerate(titles):
+        sheet.write(i,0, entry, xlwt.easyxf('bold on'))
+    for j, column in enumerate(columns):
+        for i, entry in enumerate(column):
+            sheet.write(i,j,entry)
+    workbook.save(filename)
+
+###data manipulations
+
+#subsets data (every nth entry)
 def subset(set,n):
     sub=[]
     for i in range(len(set)):
@@ -8,6 +54,7 @@ def subset(set,n):
             sub=sub+[set[i]]
     return sub
 
+#sphere geometry
 class sphere:
     def __init__(self,d):
         self.d=d
@@ -16,7 +63,7 @@ class sphere:
         self.sarea=4.0*self.xsect
         self.vol=(4.0/3.0)*pi*self.r**3.0
 
-#heat transfer params
+#a shit-ton of heat transfer params
 class htparams:
     def __init__(self,**args):
         self.g=args.get('g',9.81)
@@ -27,34 +74,37 @@ class htparams:
         self.Tinf=args.get('Tinf',273.15)
 
         self.kvisc=args.get('kvisc',False)
-        self.rho=args.get('rho',False)
+        self.rho_f=args.get('rho_f',False)
+        self.rho_s=args.get('rho_s',False)
         self.dvisc=args.get('dvisc',False)
-        if(not self.kvisc and self.rho and self.dvisc):
-            self.kvisc=self.dvisc/self.rho
-        if(not self.dvisc and self.rho and self.kvisc):
-            self.dvisc=self.kvisc*self.rho
-        self.k=args.get('k',False)
-        self.cp=args.get('cp',False)
+        if(not self.kvisc and self.rho_f and self.dvisc):
+            self.kvisc=self.dvisc/self.rho_f
+        if(not self.dvisc and self.rho_f and self.kvisc):
+            self.dvisc=self.kvisc*self.rho_f
+        self.k_f=args.get('k_f',False)
+        self.k_s=args.get('k_s',False)
+        self.cp_f=args.get('cp_f',False)
+        self.cp_s=args.get('cp_s',False)
         self.alpha=args.get('alpha',False)
-        if (not self.alpha and self.rho and self.cp and self.k):
-            self.alpha=self.k/(self.rho*self.cp)
+        if (not self.alpha and self.rho_f and self.cp_f and self.k_f):
+            self.alpha=self.k_f/(self.rho_f*self.cp_f)
         self.h=args.get('h',False)
         self.emiss=args.get('emiss',1.0)
-        self.thermexp=args.get('thermexp',1.0)
+        self.thermexp=args.get('thermexp',1.0/self.Tinf)
 
-#    def biot(self)
-#        return h*l/k
+    def biot(self)
+        return h*l/k_s
+
+    def nusselt(self)
+        return h*l/k_f
 
     def rayleigh(self,Tf):
-        print 'Ra=', self.grashof(Tf)*self.prandtl(Tf)
         return self.grashof(Tf)*self.prandtl(Tf)
 
     def grashof(self,T):
-        print 'Gr=', self.g * self.thermexp * (T-self.Tinf)*(self.l**3.0)/(self.kvisc**2)
-        return (self.g*self.thermexp*(T-self.Tinf)*self.l**3.0)/(self.kvisc**2)
+        return (self.g*self.thermexp*(T-self.Tinf)*((self.l)**3.0))/((self.kvisc)**2.0)
 
     def prandtl(self,T):
-        print 'Pr=', self.kvisc/self.alpha
         return self.kvisc/self.alpha
 
     def reynolds(self):
@@ -66,12 +116,29 @@ class htparams:
     #specific to metal sphere
     def hc(self,Tzero,Tfinal):
         #Incropera and Dewitt's empirical equation for hc
-        #May depend on Englilish units.
         Tf=self.Tfilm(Tzero,Tfinal)
-        return (self.k/self.l)*((0.589*self.rayleigh(Tf)**0.25)/(1+(0.469/self.prandtl(Tf))**(9.0/16.0))**(4.0/9.0)+2)
+        return (2+(0.589*(self.rayleigh(Tf))**0.25)/(1+(0.469/self.prandtl(Tf))**(9.0/16.0))**(4.0/9.0))*(self.k_f/self.l)
 
     def hr(self,Tsurf):
         #Don't forget to use absolute temps!
-        stefboltz=5.6704e-5/1055.0559/1550.0031/1.8 #thx units as well
-        #thx http://en.wikipedia.org/wiki/Stefan%E2%80%93Boltzmann_constant
+        stefboltz=0.1714e-8/3600.0 #courtesy cengel
         return self.emiss*stefboltz*(Tsurf**4.0-self.Tinf**4.0)/(Tsurf-self.Tinf)
+
+#returns viscosity in centipoise based on params from DV-1 viscometer
+def dv2cP(reading,model,number,rpm):
+    #pg.3-7 of manual-->excel-->csv-->gnumeric-->csv-->ctrl-c,ctrl-v >_<;;;
+    table={'RV': [[200,100,50,40,25,20,10,5,2,1],
+    [800,400,200,160,100,80,40,20,8,4],
+    [2000,1000,500,400,250,200,100,50,20,10],
+    [4000,2000,1000,800,500,400,200,100,40,20],
+    [8000,4000,2000,1600,1000,800,400,200,80,40],
+    [20000,10000,5000,4000,2500,2000,1000,500,200,100],
+    [80000,40000,20000,16000,10000,8000,4000,2000,800,400]],
+    'LV': [[200,100,40,20,10,5,2,1],
+    [1000,500,200,100,50,25,10,5],
+    [4000,2000,800,400,200,100,40,20],
+    [20000,10000,4000,2000,1000,500,200,100]]}
+
+    return reading*table[model][number-1][ \\
+    [0.5,1.,2.,2.5,4.,5.,10.,20.,50.,100.].index(rpm)]
+
