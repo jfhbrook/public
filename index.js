9400fda567ebacdf2fc26215be8355f122a03e7c
Joshua Holbrook <josh.holbrook@gmail.com>
2013-06-16 17:23:56 -0800

    [refactor] Use new "godot-producer" library

--- index.js
+++ index.js
@@ -1,49 +1,34 @@
 var path = require('path'),
     util = require('util');
 
-var Producer = require('godot').producer.Producer,
+var producer = require('godot-producer'),
     tail = require('./tail');
 
-var LogProducer = module.exports = function (options) {
-  if (!(this instanceof LogProducer)) {
-    return new LogProducer(options);
+var LogProducer = module.exports = producer(
+  function constructor(options) {
+    var self = this;
+
+    this.logs = [];
+
+    var s = tail(path.resolve(options.file));
+
+    s.on('data', function (lines) {
+      lines = lines.toString().split('\n');
+
+      if (lines[lines.length - 1] == '') {
+        lines.pop();
+      }
+
+      self.logs = self.logs.concat(lines);
+    })
+  },
+  function produce() {
+    this.values.metric = 1000 * this.logs.length / this.values.ttl;
+
+    this.emit('data', {
+      meta: { logs: this.logs }
+    });
+
+    this.logs = [];
   }
-
-  var self = this;
-
-  Producer.call(this, options);
-
-  this.logs = [];
-
-  var s = tail(path.resolve(options.file));
-
-  s.on('data', function (lines) {
-    lines = lines.toString().split('\n');
-
-    if (lines[lines.length - 1] == '') {
-      lines.pop();
-    }
-
-    self.logs = self.logs.concat(lines);
-  })
-};
-util.inherits(LogProducer, Producer);
-
-LogProducer.prototype.produce = function () {
-
-  this.values.metric = this.logs.length;
-
-  this.emit('data', {
-    host:        this.values.host,
-    service:     this.values.service,
-    state:       this.values.state,
-    time:        Date.now(),
-    description: this.values.description,
-    tags:        this.values.tags,
-    metric:      this.values.metric,
-    ttl:         this.values.ttl,
-    meta:        { logs: this.logs }
-  });
-
-  this.logs = [];
-};
+);
