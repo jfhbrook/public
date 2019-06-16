614fdd444606a3d3f9153d70fbfe21caf15a39d9
Joshua Holbrook <josh.holbrook@gmail.com>
2019-06-16 19:56:45 -0400

    Update representable decorator

--- pyxsession/util/decorators.py
+++ pyxsession/util/decorators.py
@@ -1,19 +1,7 @@
 from functools import wraps
 
 
-def representable(keys=None):
-    if not keys:
-        if hasattr(cls, '__attrs_attrs__'):
-            keys = [
-                attr.name
-                for attr in cls.__attrs_attrs__
-            ]
-        else:
-            raise TypeError(
-                'Representable classes must either be attrs classes or have '
-                'an explicit list of keys!'
-            )
-
+def add_representable_methods(cls, keys):
     def asdict(self):
         return {
             k: getattr(self, k)
@@ -22,7 +10,7 @@ def representable(keys=None):
         }
 
     def repr_(self):
-        return repr(self.as_dict())
+        return repr(self.asdict())
 
     def repr_pretty(self, p, cycle):
         if cycle:
@@ -37,13 +25,37 @@ def representable(keys=None):
                     p.text(f'{k}=')
                     p.pretty(v)
 
-    def decorator(cls):
-        # Trying to duck type attrs classes, which already have asdict
-        # and a solid repr
-        if not hasattr(cls, '__attrs_attrs__'):
-            setattr(cls, 'asdict', as_dict)
-            setattr(cls, '__repr__', repr_)
-        setattr(cls, '_repr_pretty_', repr_pretty)
-        return cls
+    # It should be possible to override this method independently of anything
+    if not hasattr(cls, 'asdict'):
+        if not keys:
+            raise TypeError(
+                'keys must be explicitly defined for classes without a '
+                'prexisting asdict method'
+            )
+        cls.asdict = asdict
 
-    return decorator
+    # attrs already has a solid repr
+    if not hasattr(cls, '__attrs_attrs__'):
+        cls.__repr__ = repr_
+
+    # Proper ipython/jupyter style pretty methods
+    cls._repr_pretty_ = repr_pretty
+
+
+def representable(cls=None):
+    if hasattr(cls, '__attrs_attrs__'):
+        keys = [attr.name for attr in cls.__attrs_attrs__]
+        add_representable_methods(cls, keys)
+        return cls
+    else:
+        if cls:
+            keys = cls
+        else:
+            keys = None
+
+        def decorator(cls):
+            add_representable_methods(cls, keys)
+            return cls
+
+        return decorator
+
