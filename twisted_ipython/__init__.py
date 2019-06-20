aef835639869090de4a57e4ddad1747cc3b14c5a
Joshua Holbrook <josh.holbrook@gmail.com>
2019-06-20 16:10:32 -0400

    Make an effort to shut down the reactor on exit

--- twisted_ipython/__init__.py
+++ twisted_ipython/__init__.py
@@ -1,9 +1,15 @@
+import atexit
 import crochet
+from twisted.internet import reactor
 from twisted_ipython.magic import install_autoawait, TwistedMagics
 
+@crochet.run_in_reactor
+def _shutdown():
+    reactor.stop()
 
 def load_ipython_extension(ipython):
     crochet.setup()
 
     ipython.register_magics(TwistedMagics)
     install_autoawait(ipython)
+    atexit.register(_shutdown)
