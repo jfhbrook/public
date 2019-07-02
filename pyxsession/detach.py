0587b0757ced2ceaa8fb1343c85b540e4ce8acc6
Joshua Holbrook <josh.holbrook@gmail.com>
2019-07-02 19:12:26 -0400

    Clean up detach

--- pyxsession/detach.py
+++ pyxsession/detach.py
@@ -2,18 +2,28 @@ import os
 import sys
 from subprocess import Popen, DEVNULL
 
-
-    # Double-forking to truly detach the process
-    # For more on the strategy, see:
-    #
-    # * https://stackoverflow.com/questions/5772873/python-spawn-off-a-child-subprocess-detach-and-exit  # noqa
-    # * https://stackoverflow.com/questions/6011235/run-a-program-from-python-and-have-it-continue-to-run-after-the-script-is-kille  # noqa
-
-
-# Our strategy is to spawn a process that then forks and execvpe's
+# Our strategy is to spawn a process piped to /dev/null that then forks and
+# execvpe's our program for us. The goal is to get a process that won't shut
+# down when we close the parent.
+#
+# This can be useful if an application is launched directly in the shell,
+# since it'll background the process and let you keep typing on your computer.
+#
+# TODO: Test behavior of X11 applications during logout. It would be really
+# funny if it doesn't handle this well.
+# TODO: Check the properties of the child process to ensure that this is
+# actually doing what I think it is!
+#
+# My favorite StackOverflow reading on how/why "double forking" is desired:
+#
+# https://stackoverflow.com/questions/881388/what-is-the-reason-for-performing-a-double-fork-when-creating-a-daemon  # noqa
 
 
 def spawn(argv, *, env=None, cwd=None):
+    # We execute a child process rather than fork here, because twisted gets
+    # very grumpy if you call os.fork while the reactor is running. See:
+    # https://stackoverflow.com/questions/13181561/python-twisted-fork-for-background-non-returning-processing  # noqa
+
     shim_argv = [sys.executable, '-m', 'pyxsession.detach'] + argv
 
     return Popen(
@@ -31,16 +41,11 @@ def _fork_and_popen(argv, env):
     if pid > 0:
         return
 
+    # A good description on StackOverflow as to why this is necessary:
+    # https://stackoverflow.com/questions/45911705/why-use-os-setsid-in-python
     os.setsid()
 
-    print(argv)
-
     cmd = argv[0]
-
-    print(cmd, argv)
-
-    print(len(argv))
-    print(bool(argv))
 
     if argv:
         os.execvpe(cmd, argv, env)
