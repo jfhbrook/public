a70b67eebd34b59f5954d5e1dda52a9713caea4c
Joshua Holbrook <josh.holbrook@gmail.com>
2019-07-04 16:50:41 -0400

    Generate a dbus schema from an attrs class

--- pyxsession/dbus/marshmallow/schema.py
+++ pyxsession/dbus/marshmallow/schema.py
@@ -1,11 +1,14 @@
 from collections import OrderedDict
 import attr
 from marshmallow import Schema, fields
+from marshmallow.schema import SchemaMeta
 from marshmallow.decorators import post_dump, pre_load, post_load
 from txdbus import client
 
+from pyxsession.util import Symbol
 
-class DBusSchema(Schema):  
+
+class DBusSchema(Schema, metaclass=SchemaMeta):
     class Meta:
         ordered = True
       
@@ -36,3 +39,18 @@ class DBusSchema(Schema):
         if many:
             return [self.cls(*un) for un in unstructured]
         return self.cls(**unstructured)
+
+
+DBUS_SCHEMA = Symbol('attrs metadata for generating a dbus schema')
+
+
+def from_attrs(attrs_cls):
+    fields = {
+        attr.name: attr.metadata[DBUS_SCHEMA]
+        for attr in attrs_cls.__attrs_attrs__
+        if DBUS_SCHEMA in attr.metadata
+    }
+
+    fields['cls'] = attrs_cls
+
+    return type(f'{attrs_cls.__name__}Schema', (DBusSchema,), fields)
