3afbadb6667021ff23d9e5eb8831c20b4f9f8ef3
Joshua Holbrook <josh.holbrook@gmail.com>
2019-07-09 09:07:46 -0400

    Clean up kbjournal

--- korbenware/cli/journal.py
+++ korbenware/cli/journal.py
@@ -1,22 +1,15 @@
 import os
 
 import click
-
 from twisted.logger import LogLevel
 
-from korbenware.config import load_config
-from korbenware.cli.base import verbosity
-from korbenware.logger import (
-    captured, CliObserver, create_logger, greet,
-    LEVEL_BY_NAME, publisher, SYSLOG_PRIORITY_BY_LEVEL
-)
+from korbenware.logger import LEVEL_BY_NAME, SYSLOG_PRIORITY_BY_LEVEL
 
 
 @click.command(
     context_settings=dict(ignore_unknown_options=True),
     help='A thin wrapper around journalctl for loading korbenware-related journald logs'  # noqa
 )
-@verbosity
 @click.option(
     '-l', '--level',
     help='Search journald logs at or above this Twisted logger level'
@@ -24,41 +17,23 @@ from korbenware.logger import (
 @click.argument(
     'journald_args', nargs=-1, type=click.UNPROCESSED
 )
-def main(verbose, level, journald_args):
-
-    config = load_config()
-
-    log = create_logger(namespace='korbenware.cli.journal')
-
-    publisher.addObserver(CliObserver(config, verbosity=verbose))
-
-    hed = "Korby Jr's Cool Petsitter's Journald Shimmy-Shim 🦜"
-    subhed = "programmed entirely while cursing Lennart Poettering's name"
+def main(level, journald_args):
 
     argv = None
 
-    with captured(log):
-        greet(log, hed, subhed)
-        try:
-            level = LEVEL_BY_NAME[level] if level else LogLevel.info
+    level = LEVEL_BY_NAME[level] if level else LogLevel.info
 
-            priority = SYSLOG_PRIORITY_BY_LEVEL[level]
+    priority = SYSLOG_PRIORITY_BY_LEVEL[level]
 
-            syslog_priority = f'--priority={priority}'
+    syslog_priority = f'--priority={priority}'
 
-            # TODO: Dynamically look up identifier from config and/or cli arg
-            syslog_identifier = f'SYSLOG_IDENTIFIER=korbenware'
+    # TODO: Dynamically look up identifier from config and/or cli arg
+    syslog_identifier = f'SYSLOG_IDENTIFIER=korbenware'
 
-            argv = [
-                'journalctl',
-                syslog_priority,
-                syslog_identifier
-            ] + list(journald_args)
+    argv = [
+        'journalctl',
+        syslog_priority,
+        syslog_identifier
+    ] + list(journald_args)
 
-            log.info('Exec-ing {command}...', command=argv)
-        except:  # noqa
-            argv = None
-            raise
-
-    if argv:
-        os.execvpe('journalctl', argv, os.environ)
+    os.execvpe('journalctl', argv, os.environ)
