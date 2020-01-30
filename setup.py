5a3527242083dd9579e482a3a8c11f37a18ebd2f
Joshua Holbrook <josh.holbrook@gmail.com>
2020-01-30 17:05:33 -0500

    Release 0.4.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(README_md, "r") as f:
 
 setup(
     name="db_hooks",
-    version="0.3.2",
+    version="0.4.0",
     packages=find_packages(),
     include_package_data=True,
     description="A simple sqlalchemy connection configuration manager",
