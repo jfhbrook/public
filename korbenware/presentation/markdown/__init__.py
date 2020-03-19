a8428772593297cd6ddff3b12353c3e217d732e6
Joshua Holbrook <josh.holbrook@gmail.com>
2020-03-19 11:49:34 -0400

    Markdown helper now uses attr name instead of class name

--- korbenware/presentation/markdown/__init__.py
+++ korbenware/presentation/markdown/__init__.py
@@ -5,8 +5,10 @@ def is_markdownable(obj):
     return hasattr(obj, '_repr_markdown_')
 
 
-def _visit(level, obj):
-    header = f'{"#" * level} {obj.__class__.__name__}'
+def _visit(level, obj, name=None):
+    if not name:
+        name = obj.__class__.__name__
+    header = f'{"#" * level} {name}'
 
     lines = [header, '']
 
@@ -36,9 +38,9 @@ def _visit(level, obj):
 
     for k, v in markdownable_attrs:
         if is_markdownable(v):
-            lines = lines + ['', v._repr_markdown_(level + 1), '']
+            lines = lines + ['', v._repr_markdown_(level + 1, name=k), '']
         else:
-            lines = lines + _visit(level + 1, v)
+            lines = lines + _visit(level + 1, v, name=k)
 
         lines.append('')
 
@@ -48,8 +50,8 @@ def _visit(level, obj):
 def markdownable(cls):
     assert_keys(cls)
 
-    def repr_markdown(self, level=1):
-        return _visit(level, self)
+    def repr_markdown(self, level=1, name=None):
+        return _visit(level, self, name)
 
     cls._repr_markdown_ = repr_markdown
 
