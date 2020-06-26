35275d583fa14981c4808fe2e7e58c525885e739
Joshua Holbrook <josh.holbrook@gmail.com>
2020-06-26 15:26:02 -0400

    Pretty print base config

--- korbenware/cli/config.py
+++ korbenware/cli/config.py
@@ -1,4 +1,6 @@
 import click
+import crayons
+from terminaltables import DoubleTable
 from twisted.logger import LogLevel
 
 from korbenware.cli.base import async_command, verbosity
@@ -12,7 +14,26 @@ from korbenware.logger import (
 def _greet(log):
     hed = "Korben's Cool Petsitter's Configuration Manager 🦜"
     subhed = 'programmed entirely while Korben was screaming at the neighbors'
-    greet(hed, subhed)
+    greet(log, hed, subhed)
+
+
+class ColorCycler:
+    COLORS = ['blue', 'magenta', 'cyan']
+
+    def __init__(self):
+        self.i = -1
+
+    def __call__(self):
+        self.i += 1
+        if self.i >= len(self.COLORS):
+            self.i = 0
+
+        crayon = getattr(crayons, self.COLORS[self.i])
+
+        return lambda o: str(crayon(o))
+
+
+get_color = ColorCycler()
 
 
 @click.group()
@@ -23,7 +44,7 @@ def main(ctx, verbose):
 
     log = create_logger(namespace='krbenware.cli.config')
 
-    publisher.addObserver(CliObserver(config, verbosity=verbose + 1))
+    publisher.addObserver(CliObserver(config, verbosity=verbose))
 
     ctx.ensure_object(dict)
 
@@ -39,12 +60,27 @@ def base():
 @base.command()
 @click.pass_context
 def show(ctx):
+    config = ctx.obj['CONFIG']
     log = ctx.obj['LOGGER']
 
     with captured(log):
-        greet()
+        _greet(log)
 
-        log_config(ctx.obj['CONFIG'], LogLevel.info)
+        table = [['section', 'key', 'value']]
+
+        for section_attr in config.__attrs_attrs__:
+            section = getattr(config, section_attr.name)
+            color = get_color()
+            if hasattr(section, '__attrs_attrs__'):
+                for attribute in section.__attrs_attrs__:
+                    table.append([color(section_attr.name), color(attribute.name), color(getattr(section, attribute.name))])
+            else:
+                for k, v in section.items():
+                    table.append([color(section_attr.name), color(k), color(v)])
+
+        print('=== BASE CONFIG: ===')
+        print(DoubleTable(table).table)
+
 
 
 @base.command()
