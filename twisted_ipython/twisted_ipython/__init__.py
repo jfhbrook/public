605c6bcf5afbe171743f300e60fa569e29da0c44
Josh Holbrook <josh.holbrook@gmail.com>
2021-10-26 17:17:06 -0400

    Move db_hooks and twisted_ipython to sfw repo

--- twisted_ipython/twisted_ipython/__init__.py
+++
@@ -1,9 +0,0 @@
-import crochet
-from twisted_ipython.magic import install_autoawait, TwistedMagics
-
-
-def load_ipython_extension(ipython):
-    crochet.setup()
-
-    ipython.register_magics(TwistedMagics)
-    install_autoawait(ipython)
