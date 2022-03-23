be22e667a3e071ae6a60e51bba22f8391511a363
Josh Holbrook <josh.holbrook@gmail.com>
2022-03-23 00:46:29 -0400

    fixes to http-server-request-* modules

--- old-memes/http-server-request-ip/index.js
+++ old-memes/http-server-request-ip/index.js
@@ -3,9 +3,3 @@ module.exports = function requestIPAddress(req) {
     || req.headers["x-forwarded-for"]
     || req.client.remoteAddress );
 };
-
-var getAccept = function (req) {
-  //I found this regexp on stack overflow.
-  //It's no real parser but probably good enough.
-  return req.headers.accept.match(/([^()<>@,;:\\"\/[\]?={} \t]+)\/([^()<>@,;:\\"\/[\]?={} \t]+)/)[0];
-}
