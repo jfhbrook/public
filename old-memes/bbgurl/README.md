dcb56e55241d29b6bd0c24432433038317b9aee1
Josh Holbrook <josh.holbrook@gmail.com>
2022-03-24 00:18:42 -0400

    release bbgurl

---
+++ old-memes/bbgurl/README.md
@@ -0,0 +1,119 @@
+# bbgurl
+
+a command line http client built on top of [udici](https://npm.im/udici).
+
+## install
+
+bbgurl is distributed on npm. you can install it with `npm i -g bbgurl`, or
+use it with npx:
+
+```bash
+npx bbgurl https://google.com
+```
+
+## usage
+
+here's the current output of the help, which I think is uncharacteristically
+comprehensive:
+
+```
+[♥] bbgurl: a cli http client using udici
+[♥] 
+[♥] USAGE: /home/josh/.nvm/versions/node/v16.5.0/bin/node [URL]
+[♥] 
+[♥] make an HTTP request using udici.request.
+[♥] 
+[♥] most undici options are bled straight through the args, using conventions
+[♥] from the optimist and minimist libraries.
+[♥] 
+[♥] for more information on udici, see: https://npm.im/udici
+[♥] 
+[♥] OPTIONS:
+[♥]     --help                   print this message.
+[♥] 
+[♥]     -Q/--quiet               suppress logging.
+[♥] 
+[♥]     -X/--method METHOD       an http method. defaults to GET.
+[♥] 
+[♥]     --body BODY              the request body. if the first character is a @,
+[♥]                              will read from file. defaults to stdin.
+[♥] 
+[♥]     --headers HEADERS        a JSON blob representing an undici headers
+[♥]                              argument. defaults to null.
+[♥] 
+[♥]     --upgrade UPGRADE        optionally upgrade the request with the specified
+[♥]                              upgrade type.
+[♥] 
+[♥]     --bodyTimeout MILLIS     how long to wait for the body before timing out.
+[♥]                              defaults to 30 seconds.
+[♥] 
+[♥]     --headersTimeout MILLIS  how long to wait for the headers before timing
+[♥]                              out. defaults to 30 seconds.
+[♥] 
+[♥]     --maxRedirections N      the maximum number of redirects to follow.
+[♥] 
+[♥]     --mixin MIXIN            if specified, call the appropriate fetch mixin on the body.
+[♥] 
+[♥]     -i/--include             include response status, headers and trailers.
+[♥] 
+[♥]     --logfile FILE           an optional file to write logs to.
+[♥] 
+[♥]     -o/--output FILE         a file to write the response to. defaults to stdout.
+[♥] 
+[♥]     -U/--user CREDENTIALS    specify basic auth credentials. ex: '-u user:pass'
+[♥] 
+```
+
+## examples:
+
+### hit an endpoint
+
+```
+$ bbgurl https://google.com
+[♥] Downloading: ======================================== 100% (220/220)
+<HTML><HEAD><meta http-equiv="content-type" content="text/html;charset=utf-8">
+<TITLE>301 Moved</TITLE></HEAD><BODY>
+<H1>301 Moved</H1>
+The document has moved
+<A HREF="https://www.google.com/">here</A>.
+</BODY></HTML>
+```
+
+### set some headers
+
+(this example does *not* work and will be updated as soon as I write tests.
+the api is still like this though!)
+
+```
+$ bbgurl http://whatismyip.jit.su -H '{ "accept": "application/json" }'
+{ "ip": "71.198.76.200" }
+```
+
+### do some basic auth
+
+(this example also needs to be updated, but the functionality hasn't changed
+here at all so it should work!)
+
+```
+$ bbgurl -u josh:supersekritpw http://localhost:8080
+{ "success": true, "message": "Welcome to the inner sanctum." }
+```
+
+### download a tarball
+
+(yes, this ancient url still works!! this is an up-to-date example!)
+
+```
+$ bbgurl --verbose http://nodejs.org/dist/v0.8.4/node-v0.8.4.tar.gz -o node-v0.8.4.tar.gz 
+[♥] data written to /home/josh/joshiverse/old-memes/bbgurl/node-v0.8.4.tar.gz
+```
+
+## what happened to the request wrapper?
+
+a previous version of this client was written as a thin wrapper around
+[mikeal/request](https://github.com/mikeal/request); however, request is
+no longer maintained, and I saw no reason not to upgrade.
+
+## license
+
+MIT/X11.
---
+++ old-memes/bbgurl/index.js
@@ -0,0 +1,252 @@
+#!/usr/bin/env node
+
+const minimist = require('minimist');
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const util = require('util');
+const url = require('url');
+
+const colors = require('ansi-colors');
+const progress = require('stream-progressbar');
+
+const HELP = `bbgurl: a cli http client using udici
+
+USAGE: ${process.argv[0]} [URL]
+
+make an HTTP request using udici.request.
+
+most undici options are bled straight through the args, using conventions
+from the optimist and minimist libraries.
+
+for more information on udici, see: https://npm.im/udici
+
+OPTIONS:
+    --help                   print this message.
+
+    -Q/--quiet               suppress logging.
+
+    -X/--method METHOD       an http method. defaults to GET.
+
+    --body BODY              the request body. if the first character is a @,
+                             will read from file. defaults to stdin.
+
+    --headers HEADERS        a JSON blob representing an undici headers
+                             argument. defaults to null.
+
+    --upgrade UPGRADE        optionally upgrade the request with the specified
+                             upgrade type.
+
+    --bodyTimeout MILLIS     how long to wait for the body before timing out.
+                             defaults to 30 seconds.
+
+    --headersTimeout MILLIS  how long to wait for the headers before timing
+                             out. defaults to 30 seconds.
+
+    --maxRedirections N      the maximum number of redirects to follow.
+
+    --mixin MIXIN            if specified, call the appropriate fetch mixin on the body.
+
+    -i/--include             include response status, headers and trailers.
+
+    --logfile FILE           an optional file to write logs to.
+
+    -o/--output FILE         a file to write the response to. defaults to stdout.
+
+    -U/--user CREDENTIALS    specify basic auth credentials. ex: '-u user:pass'
+`;
+
+function parseArgs(argv) {
+  const opts = minimist(argv, {
+    string: [
+      'method',
+      'body',
+      'headers',
+      'upgrade',
+      'bodyTimeout',
+      'headersTimeout',
+      'maxRedirections',
+
+      'mixin',
+      'logfile',
+      'output',
+      'user'
+    ],
+    boolean: [
+      'idempotent',
+      'blocking',
+
+      'help',
+      'quiet',
+      'verbose',
+      'include'
+    ],
+    alias: {
+      method: 'X',
+      body: 'd',
+      headers: 'H',
+      include: 'i',
+      output: 'o',
+      user: 'u',
+      quiet: 'Q'
+    },
+    default: {
+      include: false,
+      verbose: true
+    }
+  });
+
+  let _url = url.parse(opts._.join(' '));
+
+  if (opts.user) {
+    _url.auth = opts.user;
+  }
+
+  const undiciOptions = {
+    method: opts.method,
+    headers: opts.headers ? JSON.parse(opts.headers) : undefined,
+    idempotent: opts.idempotent,
+    blocking: opts.blocking,
+    bodyTimeout: opts.bodyTimeout ? parseInt(opts.bodyTimeout, 10) : undefined,
+    headerstimeout: opts.headersTimeout ? parseInt(opts.headersTimeout, 10) : undefined,
+    maxRedirections: opts.maxRedirections ? parseInt(opts.maxRedirections, 10) : undefined
+  };
+
+  if (opts.body && opts.body[0] === '@' && fs.existsSync(opts.body.slice(1))) {
+    undiciOptions.body = fs.createReadStream(opts.body.slice(1));
+  } else if (opts.body) {
+    undiciOptions.body = opts.body;
+  } else if (opts.method && opts.method !== 'GET') {
+    undiciOptions.body = process.stdin;
+  }
+
+  const appOptions = {
+    output: opts.output ? fs.createWriteStream(opts.output) : process.stdout,
+    outputFile: opts.output ? path.resolve(opts.output) : null,
+    help: opts.help,
+    quiet: opts.quiet,
+    verbose: !opts.quiet,
+    include: opts.include,
+    logfile: opts.logfile,
+    mixin: opts.mixin
+  };
+
+  return [[ _url, undiciOptions], appOptions ];
+}
+
+class IOManager {
+  constructor(opts) {
+    this.output = opts.output;
+
+    this._print = () => {};
+
+    if (opts.verbose && !opts.logfile) {
+      this._log = console.error;
+    } else if (opts.logfile) {
+      this._logfile = fs.createWriteStream(path.resolve(opts.logfile));
+      this._log = (message, ...params) => {
+        this._logfile.write(util.format(message, ...params) + '\n');
+      };
+    }
+
+    if (opts.outputFile !== process.stdin) {
+      this.output.on('close', () => {
+        this.log('data written to %s', opts.outputFile);
+      });
+    }
+  }
+
+  log(message, ...params) {
+    this._log(`[${colors.magenta('♥')}] ${message}`, ...params);
+  }
+
+  printLn(message, ...params) {
+    this.output.write(
+      (
+        message
+        ? util.format(message, ...params)
+        : ''
+      ) + '\r\n'
+    );
+  }
+
+  help() {
+    HELP.split('\n').forEach((line) => {
+      this.log(line);
+    });
+  }
+
+  printStatus(statusCode) {
+    this.printLn(`HTTP ${statusCode} ${http.STATUS_CODES[statusCode]}`);
+  }
+
+  printHeaders(headers) {
+    Object.entries(headers).forEach(([key, value]) => {
+      this.printLn('%s: %s', key, value);
+    });
+    this.printLn();
+  }
+}
+
+async function main() {
+  const [[url, undiciOpts], appOpts] = parseArgs(process.argv.slice(2));
+  const io = new IOManager(appOpts);
+
+  if (appOpts.help) {
+    io.help();
+    return;
+  }
+
+  const undici = await import('undici');
+
+  const showProgress = !appOpts.mixin && appOpts.verbose;
+
+  const {
+    statusCode,
+    headers,
+    trailers,
+    body
+  } = await undici.request(url, undiciOpts);
+
+  if (appOpts.include) {
+    io.printStatus(statusCode);
+    io.printHeaders(headers);
+  }
+
+  let total = null;
+
+  try {
+    total = parseInt(headers['content-length'], 10);
+  } catch (err) {}
+
+  if (appOpts.mixin) {
+    io.printLn(await body[appOpts.mixin]());
+    printTrailers();
+  } else {
+    let res = body;
+    if (showProgress && total) {
+      res = res.pipe(
+        progress(
+          `[${colors.magenta('♥')}] Downloading: :bar :percent (:current/:total)`,
+          { total, width: 40 }
+        )
+      );
+    }
+
+    res.pipe(appOpts.output);
+
+    res.on('end', printTrailers);
+  }
+
+  function printTrailers() {
+    if (appOpts.include) {
+      io.printHeaders(trailers);
+    }
+  }
+}
+
+module.exports = {
+  parseArgs,
+  IOManager,
+  main
+};
