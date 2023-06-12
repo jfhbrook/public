f97fcad4537f9f630b2be4d49b6fd06406ffa234
Josh Holbrook <josh.holbrook@gmail.com>
2023-06-11 21:32:59 -0800

    Add sources to out files

--- packages/yq/copr.sh
+++ packages/yq/copr.sh
@@ -9,14 +9,13 @@ version="$(cat "${spec}"  | grep -E '^Version: ' | sed 's/^Version: //')"
 
 mkdir -p "${sources}"
 
-# rpmbuild expects source code to be nested in this folder
-mkdir -p "src/yq-${version}"
+mkdir -p "src"
 
 # Note, even though this is a "source rpm", we're downloading pre-compiled
 # binaries. That's OK - source vs binary has more to do with the rpm lifecycle
 # than whether or not it's text. As long as we mark it as arch-specific in the
 # spec, we'll be OK.
-(cd "src/yq-${version}" && curl -L "https://github.com/mikefarah/yq/releases/download/v${version}/yq_linux_amd64.tar.gz" | tar -xz)
+(cd "src" && curl -L "https://github.com/mikefarah/yq/releases/download/v${version}/yq_linux_amd64.tar.gz" | tar -xz)
 
 # rpmbuild expects sources to be in tarballs here
 (cd src && tar -czf "${sources}/yq-${version}-x86_64.tar.gz" .)
@@ -26,3 +25,4 @@ rpmbuild --define "_topdir ${builddir}" -bs "${spec}"
 
 # put the source rpm where copr can find it + do the binary rpms
 cp "${srpms}/yq-${version}"*'.src.rpm' "${outdir}/"
+cp "${sources}/yq-${version}-x86_64.tar.gz" "${outdir}/"
