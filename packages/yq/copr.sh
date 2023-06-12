7ba2707578a3500a997d6e467fa02a465955eac7
Josh Holbrook <josh.holbrook@gmail.com>
2023-06-11 20:36:08 -0800

    I believe this will put the source rpm where its expected

--- packages/yq/copr.sh
+++ packages/yq/copr.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
-builddir="${builddir:-$(rpmbuild --eval '%{_topdir}')}/"
-sources="${builddir}SOURCES/"
+builddir="${builddir:-$(rpmbuild --eval '%{_topdir}')}"
+sources="${builddir}/SOURCES"
+srpms="${builddir}/SRPMS"
 version="$(cat "${spec}"  | grep -E '^Version: ' | sed 's/^Version: //')"
 
 mkdir -p "${sources}"
 
 curl -L "https://github.com/mikefarah/yq/releases/download/v${version}/yq_linux_amd64.tar.gz" -o "${sources}/yq-${version}-x86_64.tar.gz"
 
-rpmbuild --define "_topdir ${builddir}" --define "_rpmdir ${outdir}" -bs "${spec}"
+rpmbuild --define "_topdir ${builddir}" -bs "${spec}"
+
+cp "${srpms}/yq-${version}-x86_64.tar.gz" "${outdir}/
