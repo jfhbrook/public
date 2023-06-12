90f261d525d0383ac1469e9b478e8533eb0bd9d5
Josh Holbrook <josh.holbrook@gmail.com>
2023-06-11 19:53:54 -0800

    Do not move source rpm afterwards

--- packages/yq/copr.sh
+++ packages/yq/copr.sh
@@ -3,14 +3,10 @@
 
 builddir="${builddir:-$(rpmbuild --eval '%{_topdir}')}/"
 sources="${builddir}SOURCES/"
-srpms="${builddir}SRPMS/"
 version="$(cat "${spec}"  | grep -E '^Version: ' | sed 's/^Version: //')"
 
 mkdir -p "${sources}"
 
 curl -L "https://github.com/mikefarh/yq/releases/download/v${version}/yq_linux_amd64.tar.gz" -o "${sources}/yq_linux_amd64.tar.gz"
 
-# rpmbuild --define "_topdir ${builddir}" -bs "${spec}"
-rpmbuild --define "_topdir ${builddir}" -bs "${spec}"
-
-mv "${srpms}"yq*.src.rpm "${outdir}"
+rpmbuild -bs "${spec}"
