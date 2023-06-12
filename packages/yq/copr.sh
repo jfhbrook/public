0064e21d4850bd0e2d140d5400d24a76f47639df
Josh Holbrook <josh.holbrook@gmail.com>
2023-06-11 19:38:55 -0800

    builddir fixes

--- packages/yq/copr.sh
+++ packages/yq/copr.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
 
-topdir="$(pwd)/target/"
-sources="${topdir}SOURCES/"
-srpms="${topdir}SRPMS/"
+builddir="${builddir:-$(rpmbuild --eval '%{_topdir}')}/"
+sources="${builddir}SOURCES/"
+srpms="${builddir}SRPMS/"
 version="$(cat "${spec}"  | grep -E '^Version: ' | sed 's/^Version: //')"
 
 mkdir -p "${sources}"
 
 curl -L "https://github.com/mikefarh/yq/releases/download/v${version}/yq_linux_amd64.tar.gz" -o "${sources}/yq_linux_amd64.tar.gz"
 
-rpmbuild --define "_topdir ${topdir}" -bs "${spec}"
+# rpmbuild --define "_topdir ${builddir}" -bs "${spec}"
+rpmbuild --define "_topdir ${builddir}" -bs "${spec}"
 
 mv "${srpms}"yq*.src.rpm "${outdir}"
-rm -rf "${topdir}"
