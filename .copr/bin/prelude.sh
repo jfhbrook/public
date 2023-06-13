90044cc02e392035cea656057b819f4b903ad6cc
Josh Holbrook <josh.holbrook@gmail.com>
2023-06-13 10:09:15 -0800

    Test build of yq completes successfully!

--- .copr/bin/prelude.sh
+++ .copr/bin/prelude.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
-copr_home="$(dirname "$(dirname "$(readlink "${BASH_SOURCE[0]}")")")"
+copr_home="$(dirname "$(dirname "$(realpath "${BASH_SOURCE[0]}")")")"
 
-export builddir="${builddir:-$(rpmbuild --eval '%{_topdir}')}"
+export topdir="${topdir:-$(rpmbuild --eval '%{_topdir}')}"
+export sourcedir="${sourcedir:-$(rpmbuild --define "_topdir ${topdir}" --eval '%{_sourcedir}')}"
 export sourcedir="${sourcedir:-$(rpmbuild --eval '%{_sourcedir}')}"
 export downloaddir="${downloaddir:-$(pwd)/downloads}"
 export PATH="${copr_home}/bin:${PATH}"
 
-mkdir -p "${builddir}"
+mkdir -p "${topdir}"
 mkdir -p "${sourcedir}"
 mkdir -p "${downloaddir}"
--- packages/yq/copr/srpm.sh
+++ packages/yq/copr/srpm.sh
@@ -4,5 +4,7 @@ set -euxo pipefail
 
 source ../../.copr/bin/prelude.sh
 
+echo "${PATH}"
+
 download-sources
 build-srpm
