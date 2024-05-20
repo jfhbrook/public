dcbda53e0a2923a921c0e10c82bddf85fcae087e
Josh Holbrook <josh.holbrook@gmail.com>
2024-05-19 16:13:17 -0800

    Add arm role receivers to action group

--- verbtionary/infrastructure/observability.tf
+++ verbtionary/infrastructure/observability.tf
@@ -22,6 +22,23 @@ resource "azurerm_monitor_action_group" "verbtionary" {
     name          = "failure-emails"
     email_address = var.alert_email
   }
+
+  // These roles appear built-in. Without these, a new action group gets
+  // created with these roles.
+  //
+  // I haven't torn down and stood up the infrastructure to test if having
+  // this stops the automatic creation of that action group, but I think
+  // there's a good chance...
+  arm_role_receiver {
+    name                    = "Monitoring Contributor"
+    role_id                 = "749f88d5-cbae-40b8-bcfc-e573ddc772fa"
+    use_common_alert_schema = true
+  }
+  arm_role_receiver {
+    name                    = "Monitoring Reader"
+    role_id                 = "43d0d8ad-25c7-4714-9337-8ba259a9fe05"
+    use_common_alert_schema = true
+  }
 }
 
 resource "azurerm_monitor_smart_detector_alert_rule" "failure" {
