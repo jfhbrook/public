cad7ee98ba163c3d3deb4c2a8c53fd72ef644ee2
Josh Holbrook <josh.holbrook@gmail.com>
2024-12-03 11:14:47 -0900

    beak grindies

--- korben/src/lib.rs
+++ korben/src/lib.rs
@@ -24,7 +24,7 @@ impl Budgie {
         if burbles.len() > 0 {
             burbles
         } else {
-            String::from("...")
+            String::from("*beak grindies*")
         }
     }
 }
