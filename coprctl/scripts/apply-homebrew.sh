db138589fec4060e83dd90a4897215a572d921da
Josh Holbrook <josh.holbrook@gmail.com>
2024-12-16 10:35:49 -0900

    Attempt to publish coprctl to homebrew tap

---
+++ coprctl/scripts/apply-homebrew.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+VERSION="${1}"
+PATCH="${2}"
+
+SHA="$(sha256sum "coprctl-${VERSION}.tar.gz" | cut -d ' ' -f 1)"
+
+if [ -n "${HOMEBREW_TAP}" ]; then
+  for template in ls ./templates/*.njk; do
+    FORMULA="$(exercise-bike ./templates/${template} \
+      --version "${VERSION}" \
+      --patch "${PATCH}" \
+      --sha "${SHA}")"
+    FILENAME="$(basename -s '.njk' "${template}")"
+
+      echo "${FORMULA}" > "${HOMEBREW_TAP}/Formula/${FILENAME}"
+      (cd "${HOMEBREW_TAP}" && git add "Formula/${FILENAME}")
+  done
+
+  # (cd "${HOMEBREW_TAP}" && git commit -m "coprctl v${VERSION}-${PATCH}")
+  # (cd "${HOMEBREW_TAP}" && git push)
+else
+  echo "To automatically commit and push the formula, set the HOMEBREW_TAP environment variable."
+fi
