73f72d3786728fff58af1aa44956e16e7fda77dd
Josh Holbrook <josh.holbrook@gmail.com>
2025-03-11 09:33:40 -0800

    General shape of CLI

---
+++ nopkg/src/main.rs
@@ -0,0 +1,61 @@
+use anyhow::Result;
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    // Add a new resource
+    Add,
+    // Work with the artifact cache
+    Cache {
+        #[command(subcommand)]
+        command: CacheCommand,
+    },
+    // Generate a shell completion script
+    Completion,
+    // Initialize a new project
+    Init,
+    // Install configured resources
+    Install,
+    // Update configured resources
+    Update,
+    // Show installed resources
+    Show,
+    // Remove a resource
+    Remove,
+}
+
+#[derive(Subcommand)]
+enum CacheCommand {
+    // Add,
+    Clean,
+    List,
+    // Verify,
+}
+
+fn main() -> Result<()> {
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Commands::Add => Ok(()),
+        Commands::Cache { command } => match &command {
+            CacheCommand::Clean => Ok(()),
+            CacheCommand::List => Ok(()),
+        },
+        Commands::Completion => {
+            // TODO: https://docs.rs/clap_complete/latest/clap_complete/
+            Ok(())
+        }
+        Commands::Init => Ok(()),
+        Commands::Install => Ok(()),
+        Commands::Update => Ok(()),
+        Commands::Show => Ok(()),
+        Commands::Remove => Ok(()),
+    }
+}
