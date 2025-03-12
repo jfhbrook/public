33d36c39754884d563f43ecadc19a175a698ca7f
Josh Holbrook <josh.holbrook@gmail.com>
2025-03-12 10:40:29 -0800

    Working completion

--- nopkg/src/commands/completion.rs
+++ nopkg/src/commands/completion.rs
@@ -1,7 +1,17 @@
+use std::io;
+
 use anyhow::Result;
+use clap::Command;
+use clap_complete::aot::{Shell, generate};
 
 // TODO: https://docs.rs/clap_complete/latest/clap_complete/
-pub(crate) fn completion_command() -> Result<()> {
-    unimplemented!("completion");
+pub(crate) fn completion_command(shell: Option<Shell>, command: &mut Command) -> Result<()> {
+    let generator = shell.unwrap_or(Shell::Bash);
+    generate(
+        generator,
+        command,
+        command.get_name().to_string(),
+        &mut io::stdout(),
+    );
     Ok(())
 }
