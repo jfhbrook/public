92fcd280e902407240f54f16f0dd7039798245e7
Josh Holbrook <josh.holbrook@gmail.com>
2025-03-12 13:21:22 -0800

    Get manifest by path

--- nopkg/src/commands/add.rs
+++ nopkg/src/commands/add.rs
@@ -1,6 +1,16 @@
 use anyhow::Result;
+use camino::Utf8Path;
 
-pub(crate) fn add_command(url: &String, file: &Option<String>, unpack: &bool) -> Result<()> {
+use crate::manifest::{Manifest, get_manifest};
+
+pub(crate) fn add_command(
+    url: &String,
+    file: &Option<String>,
+    unpack: &bool,
+    manifest_path: &String,
+) -> Result<()> {
+    let manifest_path = Utf8Path::new(manifest_path);
+    let manifest = get_manifest(manifest_path)?;
     println!("url: {}", url);
     println!("file: {:?}", file);
     println!("unpack: {}", unpack);
--- nopkg/src/commands/init.rs
+++ nopkg/src/commands/init.rs
@@ -1,22 +1,11 @@
 use anyhow::Result;
-use camino::{Utf8Path, Utf8PathBuf};
+use camino::Utf8Path;
 
 use crate::init::init_manifest;
+use crate::manifest::manifest_path;
 
-fn full_path(path: &Utf8Path) -> Utf8PathBuf {
-    match path.extension() {
-        Some(ext) if ext == "toml" => path.to_path_buf(),
-        _ => {
-            let mut buf = path.to_path_buf();
-            buf.push("unpkg.toml");
-            buf
-        }
-    }
-}
-
-pub(crate) fn init_command(path: &Option<String>, overwrite: bool) -> Result<()> {
-    let path = path.clone().unwrap_or("./unpkg.toml".to_string());
-    let path = Utf8Path::new(&path);
-    let path = full_path(path);
-    init_manifest(path.as_path(), overwrite)
+pub(crate) fn init_command(path: &String, overwrite: bool) -> Result<()> {
+    let path = Utf8Path::new(path);
+    let path = manifest_path(path);
+    init_manifest(path, overwrite)
 }
