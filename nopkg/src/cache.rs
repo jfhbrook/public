eaf638000a0c43e77b6070f36bab87fedced3439
Josh Holbrook <josh.holbrook@gmail.com>
2025-03-13 09:02:29 -0800

    Add unpacked file and repo stubs to cache

--- nopkg/src/cache.rs
+++ nopkg/src/cache.rs
@@ -4,18 +4,14 @@ use std::io;
 use std::path::{Path, PathBuf};
 
 use anyhow::Result;
-use camino::Utf8Path;
-use serde_json::Value;
 use sha3::{Digest, Sha3_256};
 use xdg::BaseDirectories;
 
-// ~/.local/cache/files
-// ~/.local/cache/repos
-// ~/.local/cache/unpacked
-
+/// A map between URL and hash
 type CacheIndex = HashMap<String, String>;
 
 fn read_index<P: AsRef<Path>>(path: P) -> Result<CacheIndex> {
+    /// Read an index file.
     let path = path.as_ref();
 
     if !path.is_file() {
@@ -25,12 +21,14 @@ fn read_index<P: AsRef<Path>>(path: P) -> Result<CacheIndex> {
     let file = fs::File::open(path)?;
     let reader = io::BufReader::new(file);
 
+    // TODO: Use SQLITE?
     let index: CacheIndex = serde_json::from_reader(reader)?;
 
     Ok(index)
 }
 
 fn write_index<P: AsRef<Path>>(path: P, index: &CacheIndex) -> Result<()> {
+    /// Write out an index file.
     let file = fs::File::open(path)?;
     let writer = io::BufWriter::new(file);
 
@@ -39,7 +37,8 @@ fn write_index<P: AsRef<Path>>(path: P, index: &CacheIndex) -> Result<()> {
     Ok(())
 }
 
-fn cache_path(dirs: &BaseDirectories) -> Result<PathBuf> {
+fn place_index(dirs: &BaseDirectories) -> Result<PathBuf> {
+    /// Get the path to the index, and create any needed directories.
     let path = dirs.place_cache_file("index.json")?;
     Ok(path)
 }
@@ -60,29 +59,57 @@ fn file_path(url: &String) -> Result<PathBuf> {
     Ok(path.to_path_buf())
 }
 
+fn unpacked_file_path(url: &String, path: &String) -> Result<PathBuf> {
+    let hash = hash_url(url)?;
+
+    let path = format!("unpacked/{}/{}", hash, path);
+    let path = Path::new(&path);
+    Ok(path.to_path_buf())
+}
+
+fn repo_path(url: &String) -> Result<PathBuf> {
+    let hash = hash_url(url)?;
+
+    let path = format!("repos/{}", hash);
+    let path = Path::new(&path);
+    Ok(path.to_path_buf())
+}
+
 pub(crate) struct Cache {
     dirs: BaseDirectories,
     index: CacheIndex,
 }
 
 impl Cache {
+    ///
+    /// A resource cache. Handles flat files, unpacked archives and git repos.
+    ///
+
     pub(crate) fn new() -> Result<Self> {
+        /// Create a new resource cache.
         let dirs = BaseDirectories::with_prefix("nopkg")?;
-        let path = cache_path(&dirs)?;
+        let path = place_index(&dirs)?;
 
         let index = read_index(&path)?;
         Ok(Cache { dirs, index })
     }
 
+    // TODO: Should these take &str?
     pub(crate) fn place_file(&self, url: &String) -> Result<PathBuf> {
         let path = file_path(url)?;
         let path = self.dirs.place_cache_file(path)?;
         Ok(path)
     }
 
+    pub(crate) fn place_unpacked_file(&self, url: &String, path: &String) -> Result<PathBuf> {
+        let path = unpacked_file_path(url, path)?;
+        let path = self.dirs.place_cache_file(path)?;
+        Ok(path)
+    }
+
     pub(crate) fn update_index(&mut self, url: &String) -> Result<()> {
         let hash = hash_url(url)?;
-        let path = cache_path(&self.dirs)?;
+        let path = place_index(&self.dirs)?;
 
         self.index.insert(url.clone(), hash);
         write_index(path, &self.index)?;
@@ -92,6 +119,12 @@ impl Cache {
 
     pub(crate) fn open_file(&self, url: &String) -> Result<fs::File> {
         let path = self.place_file(url)?;
+        let file = fs::File::open(path)?;
+        Ok(file)
+    }
+
+    pub(crate) fn open_unpacked_file(&self, url: &String, path: &String) -> Result<fs::File> {
+        let path = self.place_unpacked_file(url, path)?;
         let file = fs::File::open(path)?;
         Ok(file)
     }
