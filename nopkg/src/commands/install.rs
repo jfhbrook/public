fcc1a55af8da963c2c59a09a2c6f52e5d38cf265
Josh Holbrook <josh.holbrook@gmail.com>
2025-03-18 16:49:55 -0800

    Decrease number of warnings

--- nopkg/src/commands/install.rs
+++ nopkg/src/commands/install.rs
@@ -1,6 +1,5 @@
 use anyhow::Result;
 
 pub(crate) fn install_command() -> Result<()> {
-    unimplemented!("install");
     Ok(())
 }
--- nopkg/src/commands/show.rs
+++ nopkg/src/commands/show.rs
@@ -1,6 +1,5 @@
 use anyhow::Result;
 
 pub(crate) fn show_command() -> Result<()> {
-    unimplemented!("show");
     Ok(())
 }
